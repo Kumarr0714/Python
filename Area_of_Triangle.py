f762a1c01ec46ddd055ea9786f14ba7df7e41f89
Kumarr0714 <kumareswarr@gmail.com>
2026-02-11 15:27:20 +0530

    Initial commit

---
+++ Area_of_Triangle.py
@@ -0,0 +1,21 @@
+"""
+a = int(input('enter the value of a p:'))
+b = int(input('enter the value of b p:'))
+
+print('Adding a and b is ',a+b)
+
+print('sub a and b is ',a-b)
+
+print('Adding a and b is ',a/b)
+
+print('Adding a and b is ',a*b)
+
+"""
+
+Fist_Name = input('enter the name of the fist:')
+Fist_second = input('enter the name of the second:')
+
+full_Name = Fist_Name + Fist_second
+print('Hello',full_Name ,'welcome')
+
+
